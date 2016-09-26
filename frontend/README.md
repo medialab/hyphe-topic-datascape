6d36da5bae6026d1c7b7f3bb0c1e3d7ce68d1782
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-09-26 14:09:27 +0200

    edit text

--- frontend/README.md
+++ frontend/README.md
@@ -3,4 +3,3 @@ Angular web pages served in "app/"
 
 ## Install
 	1. Run "npm install"
-	2. edit "config-DEFAULT.js" to your configuration and rename it "config.js"
--- frontend/app/config-DEFAULT.js
+++ frontend/app/config-DEFAULT.js
@@ -1,6 +1,29 @@
 'use strict';
 
-/* Services */
+/*
+88888888ba                                                                                               
+88      "8b                                                                           ,d                 
+88      ,8P                                                                           88                 
+88aaaaaa8P'  ,adPPYba,  8b,dPPYba,   ,adPPYYba,  88,dPYba,,adPYba,    ,adPPYba,     MM88MMM  ,adPPYba,   
+88""""88'   a8P_____88  88P'   `"8a  ""     `Y8  88P'   "88"    "8a  a8P_____88       88    a8"     "8a  
+88    `8b   8PP"""""""  88       88  ,adPPPPP88  88      88      88  8PP"""""""       88    8b       d8  
+88     `8b  "8b,   ,aa  88       88  88,    ,88  88      88      88  "8b,   ,aa       88,   "8a,   ,a8"  
+88      `8b  `"Ybbd8"'  88       88  `"8bbdP"Y8  88      88      88   `"Ybbd8"'       "Y888  `"YbbdP"'   
+                                                                                                         
+                                                                                                         
+                                                                                                         
+88 88                                        ad88  88                    88           88 88              
+88 88                                       d8"    ""                    ""           88 88              
+"" ""                                       88                                        "" ""              
+      ,adPPYba,   ,adPPYba,   8b,dPPYba,  MM88MMM  88   ,adPPYb,d8       88  ,adPPYba,                   
+     a8"     ""  a8"     "8a  88P'   `"8a   88     88  a8"    `Y88       88  I8[    ""                   
+     8b          8b       d8  88       88   88     88  8b       88       88   `"Y8ba,                    
+     "8a,   ,aa  "8a,   ,a8"  88       88   88     88  "8a,   ,d88  888  88  aa    ]8I                   
+      `"Ybbd8"'   `"YbbdP"'   88       88   88     88   `"YbbdP"Y8  888  88  `"YbbdP"'                   
+                                                        aa,    ,88      ,88                              
+                                                         "Y8bbdP"     888P"               
+*/
+
 
 angular.module('app.config', [])
 
