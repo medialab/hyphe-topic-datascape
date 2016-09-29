4d8fb6715b83dc5ce137c3d4c0be996ba91fae51
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-09-29 10:51:02 +0200

    refacto functions in hope of better v8 optim

--- frontend/app/src/sigma-cloudmap-renderer.js
+++ frontend/app/src/sigma-cloudmap-renderer.js
@@ -159,52 +159,6 @@
         pix[i+2] = channels[2][i/4]
         pix[i+3] = channels[3][i/4]
       }
-
-      // From http://blog.ivank.net/fastest-gaussian-blur.html
-
-      function boxesForGauss(sigma, n) { // standard deviation, number of boxes
-      
-        var wIdeal = Math.sqrt((12*sigma*sigma/n)+1);  // Ideal averaging filter width 
-        var wl = Math.floor(wIdeal);  if(wl%2==0) wl--;
-        var wu = wl+2;
-        
-        var mIdeal = (12*sigma*sigma - n*wl*wl - 4*n*wl - 3*n)/(-4*wl - 4);
-        var m = Math.round(mIdeal);
-        // var sigmaActual = Math.sqrt( (m*wl*wl + (n-m)*wu*wu - n)/12 );
-            
-        var sizes = [];  for(var i=0; i<n; i++) sizes.push(i<m?wl:wu);
-        return sizes;
-      }
-
-      function boxBlur (scl, tcl, w, h, r) {
-        for(var i=0; i<scl.length; i++) tcl[i] = scl[i];
-        boxBlurH(tcl, scl, w, h, r);
-        boxBlurT(scl, tcl, w, h, r);
-      }
-
-      function boxBlurH (scl, tcl, w, h, r) {
-        var iarr = 1 / (r+r+1);
-        for(var i=0; i<h; i++) {
-          var ti = i*w, li = ti, ri = ti+r;
-          var fv = scl[ti], lv = scl[ti+w-1], val = (r+1)*fv;
-          for(var j=0; j<r; j++) val += scl[ti+j];
-          for(var j=0  ; j<=r ; j++) { val += scl[ri++] - fv       ;   tcl[ti++] = Math.round(val*iarr); }
-          for(var j=r+1; j<w-r; j++) { val += scl[ri++] - scl[li++];   tcl[ti++] = Math.round(val*iarr); }
-          for(var j=w-r; j<w  ; j++) { val += lv        - scl[li++];   tcl[ti++] = Math.round(val*iarr); }
-        }
-      }
-
-      function boxBlurT (scl, tcl, w, h, r) {
-        var iarr = 1 / (r+r+1);
-        for(var i=0; i<w; i++) {
-          var ti = i, li = ti, ri = ti+r*w;
-          var fv = scl[ti], lv = scl[ti+w*(h-1)], val = (r+1)*fv;
-          for(var j=0; j<r; j++) val += scl[ti+j*w];
-          for(var j=0  ; j<=r ; j++) { val += scl[ri] - fv     ;  tcl[ti] = Math.round(val*iarr);  ri+=w; ti+=w; }
-          for(var j=r+1; j<h-r; j++) { val += scl[ri] - scl[li];  tcl[ti] = Math.round(val*iarr);  li+=w; ri+=w; ti+=w; }
-          for(var j=h-r; j<h  ; j++) { val += lv      - scl[li];  tcl[ti] = Math.round(val*iarr);  li+=w; ti+=w; }
-        }
-      }
     }
 
     return renderer
@@ -448,5 +402,51 @@
 
     return this;
   };
+
+  // From http://blog.ivank.net/fastest-gaussian-blur.html
+
+  function boxesForGauss(sigma, n) { // standard deviation, number of boxes
+  
+    var wIdeal = Math.sqrt((12*sigma*sigma/n)+1);  // Ideal averaging filter width 
+    var wl = Math.floor(wIdeal);  if(wl%2==0) wl--;
+    var wu = wl+2;
+    
+    var mIdeal = (12*sigma*sigma - n*wl*wl - 4*n*wl - 3*n)/(-4*wl - 4);
+    var m = Math.round(mIdeal);
+    // var sigmaActual = Math.sqrt( (m*wl*wl + (n-m)*wu*wu - n)/12 );
+        
+    var sizes = [];  for(var i=0; i<n; i++) sizes.push(i<m?wl:wu);
+    return sizes;
+  }
+
+  function boxBlur (scl, tcl, w, h, r) {
+    for(var i=0; i<scl.length; i++) tcl[i] = scl[i];
+    boxBlurH(tcl, scl, w, h, r);
+    boxBlurT(scl, tcl, w, h, r);
+  }
+
+  function boxBlurH (scl, tcl, w, h, r) {
+    var iarr = 1 / (r+r+1);
+    for(var i=0; i<h; i++) {
+      var ti = i*w, li = ti, ri = ti+r;
+      var fv = scl[ti], lv = scl[ti+w-1], val = (r+1)*fv;
+      for(var j=0; j<r; j++) val += scl[ti+j];
+      for(var j=0  ; j<=r ; j++) { val += scl[ri++] - fv       ;   tcl[ti++] = Math.round(val*iarr); }
+      for(var j=r+1; j<w-r; j++) { val += scl[ri++] - scl[li++];   tcl[ti++] = Math.round(val*iarr); }
+      for(var j=w-r; j<w  ; j++) { val += lv        - scl[li++];   tcl[ti++] = Math.round(val*iarr); }
+    }
+  }
+
+  function boxBlurT (scl, tcl, w, h, r) {
+    var iarr = 1 / (r+r+1);
+    for(var i=0; i<w; i++) {
+      var ti = i, li = ti, ri = ti+r*w;
+      var fv = scl[ti], lv = scl[ti+w*(h-1)], val = (r+1)*fv;
+      for(var j=0; j<r; j++) val += scl[ti+j*w];
+      for(var j=0  ; j<=r ; j++) { val += scl[ri] - fv     ;  tcl[ti] = Math.round(val*iarr);  ri+=w; ti+=w; }
+      for(var j=r+1; j<h-r; j++) { val += scl[ri] - scl[li];  tcl[ti] = Math.round(val*iarr);  li+=w; ri+=w; ti+=w; }
+      for(var j=h-r; j<h  ; j++) { val += lv      - scl[li];  tcl[ti] = Math.round(val*iarr);  li+=w; ti+=w; }
+    }
+  }
 
 })(sigma);
