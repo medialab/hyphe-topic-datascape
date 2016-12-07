218612e3d6a2a34563ea7d2e63c74f9ae5f8ac32
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-12-07 16:27:09 +0100

    code cleaning

--- frontend/app/src/views/webentities.js
+++ frontend/app/src/views/webentities.js
@@ -76,18 +76,19 @@ angular.module('app.webentities', ['ngRoute'])
     query($scope.searchQuery)
   }
 
-  $scope.highlight = debounce(function(id) {
-    console.log('update', $scope.webentities.filter(function(w){return w.id==id})[0].name)
-    $scope.highlightedEntity = id
-  }, 10)
+  $scope.highlight = function(id) {
+    $timeout(function(){
+      $scope.highlightedEntity = id
+    })
+  }
 
-  // $scope.dishighlight = function(id) {
-  //   if ($scope.highlightedEntity == id) {
-  //     $timeout(function(){
-  //       $scope.highlightedEntity = undefined
-  //     })
-  //   }
-  // }
+  $scope.dishighlight = function(id) {
+    $timeout(function(){
+      if ($scope.highlightedEntity == id) {
+        $scope.highlightedEntity = undefined
+      }
+    })
+  }
 
   init()
 
@@ -180,20 +181,5 @@ angular.module('app.webentities', ['ngRoute'])
     })
     return result
   }
-
-  function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
 
 })
