33fa7c050f145545fb5b65b8e3bf0d617031e6cd
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-12-15 15:51:49 +0100

    commit conf file after all

---
+++ frontend/app/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Services */
+
+angular.module('app.config', [])
+
+.constant('solrEndpoint', 'http://jiminy.medialab.sciences-po.fr/solr/hyphe-AXA/')
+.constant('datascapeTitle', 'Privacy web corpus')
+.constant('datascapeLogos', ['res/medialab.png', 'res/axarf.png', 'res/axadil.png'])
